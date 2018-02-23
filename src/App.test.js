8922d6fb43ac96894746c6d03d64ae3d9fcd2b7f
Mnargh <tom.j.v.brandon@gmail.com>
2018-02-23 15:46:25 +0000

    Setup and configured jest-enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
+const wrapper = shallow(<App />)
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+describe ('testing App component', () => { 
+   let wrapper;  beforeEach(() => { wrapper = shallow(<App />);
+   }); 
+   
+   it('App renders as a div', () => {    
+     expect(wrapper.type()).toBe('div');  
+    });
 });
