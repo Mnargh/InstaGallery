d646c76f7cc05e074d1ee2abd0d10b84836d8d03
Mnargh <tom.j.v.brandon@gmail.com>
2018-02-25 22:37:56 +0000

    Post component passes props on to child component

--- src/tests/post.test.js
+++ src/tests/post.test.js
@@ -7,16 +7,22 @@ import Username from "../components/username";
 describe("Post component", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<Post />);
+    wrapper = shallow(<Post username={"Tom"} />);
   });
 
   it("Post renders as a div", () => {
     expect(wrapper.type()).toBe("div");
   });
 
-  // test for post receiving props/rendering props
-
   it("Post renders a username component inside of it", () => {
     expect(wrapper.find('Username')).toHaveLength(1);
+  });
+
+  it("Post passes username prop on to child components", () => {
+    // console.log(wrapper.props().children);
+    // console.log("vision");
+    // console.log(wrapper.instance().props.username);
+    // console.log(wrapper.find("Username").props());
+    expect(wrapper.instance().props.username).toEqual(wrapper.props().children.props.username);
   });
 });
